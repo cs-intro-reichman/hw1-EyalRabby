08e0d53935daa7868e5ca6a8df85161f8c267e54
EyalRabby <eyal.rabby@post.runi.ac.il>
2024-11-06 19:36:51 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,7 +13,13 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         
-		if (hours > 12) {
+		if (hours == 12) {
+			if (minutes > 10){
+				System.out.println(hours + ":" + minutes + " AM");
+				} else {
+					System.out.println(hours + ":0" + minutes + " AM");
+				}
+			} else if (hours > 12) {
 			hours -= 12;
 			if (minutes > 10){
 				System.out.println(hours + ":" + minutes + " PM");
