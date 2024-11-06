e264be5c9e0d59bbdf86709e38777a994c4201c7
EyalRabby <eyal.rabby@post.runi.ac.il>
2024-11-06 12:27:18 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -9,6 +9,6 @@ public class Bill3 {
 
 		double divided = sum / 3.0;
 
-		System.out.println("Dear " + name1  + ", " + name2 + " and " + name3 + ": pay " + Math.ceil(divided) + " Shekels each.");
+		System.out.println("Dear " + name3  + ", " + name2 + " and " + name1 + ": pay " + Math.ceil(divided) + " Shekels each.");
 	}
 }
