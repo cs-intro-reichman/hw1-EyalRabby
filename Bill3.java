af39694928ae30422df8c43193bae93842223e61
EyalRabby <eyal.rabby@post.runi.ac.il>
2024-11-06 15:00:37 +0200

    trial

--- Bill3.java
+++ Bill3.java
@@ -9,6 +9,6 @@ public class Bill3 {
 
 		double divided = sum / 3.0;
 
-		System.out.println("Dear " + name3  + ", " + name2 + " and " + name1 + ": pay " + Math.ceil(divided) + " Shekels each");
+		System.out.println("Dear " + name3  + ", " + name2 + ", and " + name1 + ": pay " + Math.ceil(divided) + " Shekels each");
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
-	}
+		double initAmount = Double.parseDouble(args[0]);
+		double interestRate = Double.parseDouble(args[1]);
+		double years =  Double.parseDouble(args[2]);
+
+		double futureValue = initAmount * Math.pow((1 + ( interestRate / 100)), years);
+
+		System.out.println("After " + (int) years + "years, " + "$" + (int) initAmount + " saved at " + (int) interestRate + "% will yield $" + (int) futureValue);
+		
+		}
 }
